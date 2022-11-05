db5618e45a328ebdfa7966c3d5b16b06bc00fe40
Albert de Paula <albert_depaula@yahoo.com.br>
2022-11-05 12:44:10 -0300

    Corrigido o script dev para utilizar o tsconfig paths

--- src/comamnds.ts
+++ src/comamnds.ts
@@ -5,12 +5,12 @@ const commands = [
     ],
 
     dev_dependencies: [
+      'yarn add typescript@4.1.2 -D ',
       'yarn add @types/node@14.14.10 -D',
       'yarn add ts-node-dev@1.0.0 -D',
-      'yarn add typescript@4.1.2 -D  ',
       'yarn add -D eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin',
       'yarn add eslint-config-prettier eslint-plugin-prettier -D',
-      'yarn add @types/express @types/cors-D',
+      'yarn add @types/express @types/cors -D',
       'yarn add --dev prettier',
     ],
 
