8eaa8fa056dc0024b580c4e260a1dbc6c1d11910
Albert_Remote <albert_depaula@yahoo.com.br>
2022-11-13 20:51:52 -0300

    Criado SessionsController e CreateSessionsService

---
+++ src/modules/users/routes/sessions.routes.ts
@@ -0,0 +1,51 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+
+const sessionsRouter = Router();
+const sessionsController = new UsersController();
+
+usersRouter.get('/', usersController.index);
+
+usersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.show,
+);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
+
+usersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.update,
+);
+
+usersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.delete,
+);
+
+export default usersRouter;
---
+++ src/modules/users/services/CreateSessionsService.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+import { compare, hash } from 'bcryptjs';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UserRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+}
+
+class CreateSessionsService {
+  public async execute({ email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const passwordconfirmed = await compare(password, user.password);
+
+    if (!passwordconfirmed) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateSessionsService;
